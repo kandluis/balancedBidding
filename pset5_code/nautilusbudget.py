7a0be7d58308966f02dd4daedfb1dbe8fa75a90d
Luis Perez <kandluis@yahoo.com>
2015-04-01 15:50:38 -0400

    NautilusBudget runs

--- pset5_code/nautilusbudget.py
+++ pset5_code/nautilusbudget.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import sys
+import math
 
 from gsp import GSP
 from util import argmax_index
 
-from nautilus import Nautilusbb
+from nautilusbb import Nautilusbb
 
 def iround(x):
     """Round x and return an int"""
@@ -15,66 +16,53 @@ class NautilusBudget(Nautilusbb):
     """Balanced bidding agent"""
     min_value = 0.25
     max_value = 1.75
-    competitors = 4.0
+    competitors = 4
     dropoff = 0.75
 
     def __init__(self, id, value, budget):
-        super(NautilusbbBudget, self).__init__(id,value)
+        super(NautilusBudget, self).__init__(id,value,budget)
         self.values_of_agents = {}
 
     def initial_bid(self, reserve):
         # initial bid is a balanced bid
         max_bid = self.max_value/2
         min_bid = self.min_value/2
-        expected_min_bid = lambda s: ((self.competitors - s) / self.competitors) * (max_bid - min_bid) + min_bid
+        expected_min_bid = lambda s: ((float(self.competitors) - s) / float(self.competitors)) * (max_bid - min_bid) + min_bid
         expected_bids = [expected_min_bid(k) for k in range(1,self.competitors+1)]
-        info = [(i,bid,bid) for  enumerate(expected_bids)]
-        info.append((self.competitors,reserve,reserve))f
+        info = [(i,bid,bid) for (i,bid) in enumerate(expected_bids)]
 
         # clicks for initial round
-        clicks = get_clicks(0)
+        clicks = self.get_clicks(0,None)
 
         # maximal utility
-        utilities = self.expected_utils_h(info, clicks)
+        utilities = self.expected_utils_h(info, clicks, self.value)
         target = argmax_index(utilities)
         p = expected_bids[target]
 
         # written for clarity
         if p >= self.value:
             bid = self.value
-        elif k > 0:
+        elif target > 0:
             # use click ratio as a measure of quality ratio
-            bid = self.value - (clicks[k]/clicks[k-1]) * (self.value - p)
+            bid = self.value - (clicks[target]/clicks[target-1]) * (self.value - p)
         else:
             bid = self.value
 
         return bid
 
-    def get_clicks(t):
+    def get_clicks(self,t, history):
         '''
         Modified to take advantage of next round clicks
         '''
         top_slot = iround(30*math.cos(math.pi*t/24) + 50)
-        return [iround(top_slot_clicks * pow(self.dropoff, i))
+        return [iround(top_slot * pow(self.dropoff, i))
                               for i in range(self.competitors)]
-
-    def target_slot(self, t, history, reserve):
-        """Figure out the best slot to target, assuming that everyone else
-        keeps their bids constant from the previous rounds.
-
-        Returns (slot_id, min_bid, max_bid), where min_bid is the bid needed to tie
-        the other-agent bid for that slot in the last round.  If slot_id = 0,
-        max_bid is min_bid * 2
-        """
-        i =  argmax_index(self.expected_utils(t, history, reserve))
-        info = self.slot_info(t, history, reserve)
-        return info[i]
 
     def get_bid(t, history, reserve, value):
         if t == 0:
             return value/2
         else:
-            return super(NautilusbbBudget,self).bid_value(t,history,reserve,value)
+            return super(NautilusBudget,self).bid_value(t,history,reserve,value)
 
     def bid(self, t, history, reserve):
         # if t = 1, we've finished t = 0 round, so save what everyone's values are
@@ -105,7 +93,7 @@ class NautilusBudget(Nautilusbb):
         else:
             easiness = 0.5
 
-        return easiness*super(NautilusbbBudget, self).bid(t,history,reserve) 
+        return easiness*super(NautilusBudget, self).bid(t,history,reserve) 
 
 
 
